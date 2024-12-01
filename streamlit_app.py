82109d1ffa457e1f52eb0fad7554a0a1bb730e2e
fnjahan <fairuzbuilds@gmail.com>
2024-12-01 00:53:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,12 @@ st.title('🤖 Machine Learning Application')
 
 st.info('This is an application that builds a machine learning model!')
 
-
+# creates a dropdown with text 'Data' and shows 'Raw data' and csv file when clicked on drop down
 with st.expander('Data'):
   st.write('**Raw Data**')
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   df
+
+st.write('**X**')
+X = df.drop('species', axis=1)
+X
