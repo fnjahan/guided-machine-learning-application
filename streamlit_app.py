eafd282f39ca026c03b30bb996344e7132402054
fnjahan <fairuzbuilds@gmail.com>
2024-12-01 00:40:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🤖 Machine Learning Application')
 
-st.write('Helloooo Bhaiyaaaa!!')
-
 st.info('This is an application that builds a machine learning model!')
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+df
