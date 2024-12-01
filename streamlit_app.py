a6d4ca5037d4d45d28d01b0da93728b0ed8a2371
fnjahan <fairuzbuilds@gmail.com>
2024-12-01 16:26:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,6 +54,8 @@ with st.expander('Input features'):
 # Encode X
 encode = ['island', 'sex']
 df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+
+X = df_penguins[1:]
 input_row = df_penguins[:1]
 
 # Encode y
@@ -72,9 +74,14 @@ with st.expander('Data preparation'):
   st.write('**Encoded y**')
   y
 
-# Model Training
+# Model Training and Inference
+## Train the ML model
+clf = RandomForestClassifier()
+clf.fir(X, y)
 
-
+## Apply model to make predictions
+prediction = clf.predict(input_row)
+prediction_proba = clf.prediction_proba(input_row)
 
 
 
