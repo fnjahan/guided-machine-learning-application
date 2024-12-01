b5b5257943e581d4b77658556786b6d2a3fa4828
fnjahan <fairuzbuilds@gmail.com>
2024-12-01 17:10:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,7 +84,7 @@ clf.fit(X, y)
 
 ## Apply model to make predictions
 prediction = clf.predict(input_row)
-prediction_proba = clf.prediction_proba(input_row)
+prediction_proba = clf.predict_proba(input_row)
 
 prediction_proba
 
